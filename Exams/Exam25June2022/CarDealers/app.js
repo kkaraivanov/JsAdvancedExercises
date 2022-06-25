ad86bccec9d1927276ee89ef767a9e74b072eec4
kkaraivanov <kostadin.karaivanov@outlook.com>
2022-06-26 00:54:41 +0300

    add exam 2022

---
+++ Exams/Exam25June2022/CarDealers/app.js
@@ -0,0 +1,63 @@
+window.addEventListener("load", solve);
+
+function solve() {
+  const inputs = {
+    make: document.getElementById('make'),
+    model: document.getElementById('model'),
+    year: document.getElementById('year'),
+    fuel: document.getElementById('fuel'),
+    originalCost: document.getElementById('original-cost'),
+    sellingPrice: document.getElementById('selling-price')
+  };
+  const table = document.getElementById('table-body');
+  const profit = document.getElementById('profit')
+
+  document.getElementById('publish').addEventListener('click', onPublish)
+  function onPublish(e){
+    e.preventDefault()
+    const isNotEmpty = Object.values(inputs).filter(e => e.value != '').length === 6
+    const priceIsValid = Number(inputs.originalCost.value) < Number(inputs.sellingPrice.value)
+    if(!isNotEmpty || !priceIsValid) return
+    const row = publish.call(document.createElement('tr'), inputs)
+    table.appendChild(row)
+  }
+
+  function publish(obj){
+    const [make, model, year, fuel, originalCost, sellingPrice] = Object.values(obj)
+    console.log(make.value)
+    this.clasName = 'row';
+    this.innerHTML = `${Object.values(obj).map(e => `<td>${e.value}</td>`).join(' ')}
+    <td>
+      <button class='action-btn edit'>Edit</button>
+      <button class='action-btn sell'>Sell</button>
+    </td>`
+    Object.values(obj).forEach(v => v.value = '');
+
+    this.lastElementChild.children[0].addEventListener('click', () => {
+      make.value = this.children[0].textContent
+      model.value = this.children[1].textContent
+      year.value = this.children[2].textContent
+      fuel.value = this.children[3].textContent
+      originalCost.value = this.children[4].textContent
+      sellingPrice.value = this.children[5].textContent
+      this.remove()
+    });
+    this.lastElementChild.children[1].addEventListener('click', () => {
+      const price = Number(this.children[5].textContent) - Number(this.children[4].textContent)
+      const li = sellCar.call(document.createElement('li'), this.children[0].textContent, this.children[1].textContent, this.children[2].textContent, price);
+      const profitPrice = Number(profit.textContent) + price;
+      document.getElementById('cars-list').appendChild(li);
+      profit.textContent = profitPrice.toFixed(2)
+      this.remove();
+    })
+    return this
+  }
+
+  function sellCar(make, model, year, price){
+    this.className = 'each-list'
+    this.innerHTML = `<span>${make} ${model}</span>
+      <span>${year}</span>
+      <span>${price}</span>`
+      return this
+  }
+}
---
+++ Exams/Exam25June2022/SmartHike/smartHike.js
@@ -0,0 +1,61 @@
+class SmartHike {
+    constructor(username) {
+        this.username = username;
+        this.goals = {};
+        this.listOfHike = [];
+        this.resources = 100;
+    }
+    addGoal(peak, altitude) {
+        if (this.goals.hasOwnProperty(peak)) {
+            return `${peak} has already been added to your goals`
+        }
+
+        this.goals[peak] = altitude
+        return `You have successfully added a new goal - ${peak}`
+    }
+    hike(peak, time, difficultyLevel) {
+        if (!this.goals.hasOwnProperty(peak)) {
+            throw new Error(`${peak} is not in your current goals`)
+        }
+        if (this.goals.hasOwnProperty(peak) && this.resources <= 0) {
+            throw new Error(`You don't have enough resources to start the hike`)
+        }
+
+        let dif = this.resources - Number(time) * 10;
+        if (dif < 0) {
+            return `You don't have enough resources to complete the hike`
+        }
+        this.resources -= Number(time) * 10;
+        this.listOfHike.push({ peak, time, difficultyLevel });
+        return `You hiked ${peak} peak for ${time} hours and you have ${dif}% resources left`
+    }
+    rest(time) {
+        this.resources += Number(time) * 10
+        if (this.resources >= 100) {
+            this.resources = 100;
+            return `Your resources are fully recharged. Time for hiking!`
+        }
+
+        return `You have rested for ${time} hours and gained ${time * 10}% resources`
+    }
+    showRecord(criteria) {
+        if (this.listOfHike.length === 0) {
+            return `${this.username} has not done any hiking yet`
+        }
+        let hiks = [];
+        if (criteria != 'all') {
+            hiks = this.listOfHike.filter(e => e.difficultyLevel == criteria).sort((x, y) => x.time - y.time)
+            if (hiks.length === 0) return `${this.username} has not done any ${criteria} hiking yet`
+
+            return `${this.username}'s best ${criteria} hike is ${hiks[0].peak} peak, for ${hiks[0].time} hours`
+        }else {
+            hiks = this.listOfHike.map(e => `${this.username} hiked ${e.peak} for ${e.time} hours`)
+            return [
+                `All hiking records:`,
+                hiks.join('\n')
+            ].join('\n')
+        }
+        
+        return 'Test'
+    }
+}
